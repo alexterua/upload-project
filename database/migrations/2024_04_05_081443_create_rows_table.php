f1526bcc9a114917c5d24b449b8d725c9d26e2b6
alexterua <alexter.od.ua@gmail.com>
2024-04-05 13:47:56 +0300

    Added File, Row and IncorrectRow models with migrations

---
+++ database/migrations/2024_04_05_081443_create_rows_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('rows', function (Blueprint $table) {
+            $table->id();
+            $table->jsonb('data');
+            $table->foreignId('file_id')->index()->constrained('files');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        if (app()->isLocal()) {
+            Schema::dropIfExists('rows');
+        }
+    }
+};
