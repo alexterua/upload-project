ce0d6ac60182359636d234661f9362d1816a8f0e
alexterua <alexter.od.ua@gmail.com>
2024-04-07 11:06:29 +0300

    Updated application login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -76,6 +76,15 @@ const submit = () => {
                 </label>
             </div>
 
+            <div class="block mt-4">
+                <Link
+                    :href="route('register')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Not registered yet?
+                </Link>
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 <Link
                     v-if="canResetPassword"
